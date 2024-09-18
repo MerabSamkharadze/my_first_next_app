9fbf8e7448008ddfcce5f166bec237a196188ce6
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-09-18 11:55:11 +0400

    add routes

---
+++ app/mebo/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default () => {
+  return (
+    <div>
+      <Link href={"/mebo/samkharadze"}>go to gvari</Link>
+    </div>
+  );
+};
---
+++ app/mebo/samkharadze/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default () => {
+  return (
+    <>
+      <div>Hello samkharadze</div>
+      <Link href={"../mebo"}>Go back</Link>
+    </>
+  );
+};
