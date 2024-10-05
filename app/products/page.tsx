7eff13c3a50fb22fa82b5af81a7c55d8207fd95a
MerabSamkharadze <Samkharadzemerab@gmail.com>
2024-10-05 22:53:13 +0400

    add axios and getproduct function

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,21 @@
-export default function page() {
-  return <div>page</div>;
+"use client";
+
+import axios from "axios";
+import { useState } from "react";
+
+export default function Page() {
+  const [posts, setPosts] = useState([]);
+
+  const getProducts = () => {
+    axios.get("https://dummyjson.com/posts").then((result) => {
+      setPosts(result.data.posts);
+    });
+  };
+  console.log(posts);
+
+  return (
+    <div>
+      <button onClick={getProducts}>get data</button>
+    </div>
+  );
 }
